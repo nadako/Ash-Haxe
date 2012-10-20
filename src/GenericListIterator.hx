425798f5757221fba275789090ebccd39a8e9880
Dan Korostelev <nadako@gmail.com>
2012-10-20 17:52:04 +0400

    Do not store next iteration value in the iterator, because it can change while iterating. Store previous value instead and check if it has something "next". Also add more static typing for the list iterator.

--- src/GenericListIterator.hx
+++ src/GenericListIterator.hx
@@ -4,24 +4,29 @@ package ;
  * An iterator class for any linked lists that
  * has "next" variable in its elements.
  **/
-class GenericListIterator<TNode>
+class GenericListIterator<TNode:HasNext<TNode>>
 {
-    public var head:TNode;
+    private var previous:HasNext<TNode>;
 
     public function new(head:TNode)
     {
-        this.head = head;
+        this.previous = {next: head};
     }
 
     public function hasNext():Bool
     {
-        return head != null;
+        return previous.next != null;
     }
 
     public function next():TNode
     {
-        var node:TNode = head;
-        head = untyped node.next;
+        var node:TNode = previous.next;
+        previous = node;
         return node;
     }
+}
+
+typedef HasNext<T> =
+{
+    var next:T;
 }
