6897fa27f26a167a06fb1cf230d1262b9695063a
Dan Korostelev <nadako@gmail.com>
2013-02-20 03:54:45 +0400

    Update ObjectHash class to latest NME one.

--- src/nme/ObjectHash.hx
+++ src/nme/ObjectHash.hx
@@ -1,14 +1,21 @@
 package nme;
 
+
 #if flash
 import flash.utils.TypedDictionary;
 #end
 
-class ObjectHash<K, T>
-{
+#if haxe3
+typedef IntHash<T> = haxe.ds.IntMap<T>;
+#end
+
+
+class ObjectHash <K, T> {
+
+
     #if flash
 	/** @private */ private var dictionary:TypedDictionary <K, T>;
-    #else
+	#else
     /** @private */ private var hashKeys:IntHash <K>;
     /** @private */ private var hashValues:IntHash <T>;
     #end
@@ -16,164 +23,155 @@ class ObjectHash<K, T>
     /** @private */ private static var nextObjectID:Int = 0;
 
 
-    public function new()
-    {
+    public function new() {
 
         #if flash
-
-		dictionary = new TypedDictionary <K, T> ();
-
-        #else
-
-        hashKeys = new IntHash <K> ();
-        hashValues = new IntHash <T> ();
-
-        #end
-
-    }
-
-
-    public inline function exists(key:K):Bool
-    {
-
-        #if flash
-
-		return dictionary.exists(key);
-
-        #else
-
-        return hashValues.exists (getID (key));
-
-        #end
-
-    }
-
-
-    public inline function get(key:K):T
-    {
-
-        #if flash
-
-		return dictionary.get(key);
-
-        #else
-
-        return hashValues.get (getID (key));
-
-        #end
-
-    }
-
-
-    /** @private */
-
-    private inline function getID(key:K):Int
-    {
-
-        #if cpp
-
-		return untyped __global__.__hxcpp_obj_id (key);
-
-		#elseif !flash
-
-		if (untyped key.___id___ == null) {
-
-			untyped key.___id___ = nextObjectID ++;
-
-			if (nextObjectID == #if neko 0x3fffffff #else 0x7fffffff #end) {
-
-				nextObjectID = 0;
-
-			}
-
-		}
-
-		return untyped key.___id___;
-
+		
+		dictionary = new TypedDictionary <K, T>();
+		
 		#else
 
-		return 0;
+        hashKeys = new IntHash <K>();
+        hashValues = new IntHash <T>();
 
         #end
 
     }
 
 
-    public inline function iterator():Iterator <T>
-    {
+    public inline function exists(key:K):Bool {
 
         #if flash
+		
+		return dictionary.exists(key);
+		
+		#else
 
-		var values:Array <T> = new Array <T> ();
+        return hashValues.exists(getID(key));
 
-        for (key in dictionary.iterator())
-        {
+        #end
 
-            values.push(dictionary.get(key));
+    }
+
+
+    public inline function get(key:K):T {
+
+        #if flash
+		
+		return dictionary.get(key);
+		
+		#else
+
+        return hashValues.get(getID(key));
+
+        #end
+
+    }
+
+
+    /** @private */ private inline function getID(key:K):Int {
+
+        #if cpp
+		
+		return untyped __global__.__hxcpp_obj_id(key);
+		
+		#elseif !flash
+		
+		if (untyped key.___id___ == null) {
+
+            untyped key.___id___ = nextObjectID ++;
+
+            if (nextObjectID == #if neko 0x3fffffff #else 0x7fffffff #end) {
+
+                nextObjectID = 0;
+
+            }
 
         }
 
-        return values.iterator();
+        return untyped key.___id___;
 
         #else
+		
+		return 0;
+		
+		#end
 
-        return hashValues.iterator ();
+    }
+
+
+    public inline function iterator():Iterator <T> {
+
+        #if flash
+		
+		var values:Array <T> = new Array <T>();
+		
+		for (key in dictionary.iterator()) {
+			
+			values.push(dictionary.get(key));
+			
+		}
+		
+		return values.iterator();
+		
+		#else
+
+        return hashValues.iterator();
 
         #end
 
     }
 
 
-    public inline function keys():Iterator <K>
-    {
+    public inline function keys():Iterator <K> {
 
         #if flash
-
+		
 		return dictionary.iterator();
+		
+		#else
 
-        #else
-
-        return hashKeys.iterator ();
+        return hashKeys.iterator();
 
         #end
 
     }
 
 
-    public inline function remove(key:K):Void
-    {
+    public inline function remove(key:K):Void {
 
         #if flash
-
+		
 		dictionary.delete(key);
+		
+		#else
 
-        #else
+        var id = getID(key);
 
-        var id = getID (key);
-
-        hashKeys.remove (id);
-        hashValues.remove (id);
+        hashKeys.remove(id);
+        hashValues.remove(id);
 
         #end
 
     }
 
 
-    public inline function set(key:K, value:T):Void
-    {
+    public inline function set(key:K, value:T):Void {
 
         #if flash
-
+		
 		dictionary.set(key, value);
+		
+		#else
 
-        #else
+        var id = getID(key);
 
-        var id = getID (key);
-
-        hashKeys.set (id, key);
-        hashValues.set (id, value);
+        hashKeys.set(id, key);
+        hashValues.set(id, value);
 
         #end
 
     }
+
+
 }
