54b68bb01cbba1ffcf15e8d4080c0fd00afd73fa
Dan Korostelev <nadako@gmail.com>
2013-02-20 02:31:08 +0400

    Add comments to the ComponentMatchingFamily and NodePool classes.

--- src/ash/core/ComponentMatchingFamily.hx
+++ src/ash/core/ComponentMatchingFamily.hx
@@ -11,13 +11,26 @@ import nme.ObjectHash;
  */
 class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
 {
+    /**
+     * The nodelist managed by this family. This is a reference that remains valid always
+     * since it is retained and reused by Systems that use the list. i.e. we never recreate the list,
+     * we always modify it in place.
+     */
     public var nodeList(default, null):NodeList<TNode>;
+
     private var entities:ObjectHash<Entity, TNode>;
     private var nodeClass:Class<TNode>;
     private var components:ObjectHash<Class<Dynamic>, String>;
     private var nodePool:NodePool<TNode>;
     private var engine:Engine;
 
+    /**
+     * The constructor. Creates a ComponentMatchingFamily to provide a NodeList for the
+     * given node class.
+     *
+     * @param nodeClass The type of node to create and manage a NodeList for.
+     * @param engine The engine that this family is managing teh NodeList for.
+     */
     public function new(nodeClass:Class<TNode>, engine:Engine)
     {
         this.nodeClass = nodeClass;
@@ -25,6 +38,10 @@ class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
         init();
     }
 
+    /**
+     * Initialises the class. Creates the nodelist and other tools. Analyses the node to determine
+     * what component types the node requires.
+     */
     private function init():Void
     {
         nodePool = new NodePool<TNode>( nodeClass );
@@ -38,16 +55,29 @@ class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
         #end
     }
 
+    /**
+     * Called by the engine when an entity has been added to it. We check if the entity should be in
+     * this family's NodeList and add it if appropriate.
+     */
     public function newEntity(entity:Entity):Void
     {
         addIfMatch(entity);
     }
 
+    /**
+     * Called by the engine when a component has been added to an entity. We check if the entity is not in
+     * this family's NodeList and should be, and add it if appropriate.
+     */
     public function componentAddedToEntity(entity:Entity, componentClass:Class<Dynamic>):Void
     {
         addIfMatch(entity);
     }
 
+    /**
+     * Called by the engine when a component has been removed from an entity. We check if the removed component
+     * is required by this family's NodeList and if so, we check if the entity is in this this NodeList and
+     * remove it if so.
+     */
     public function componentRemovedFromEntity(entity:Entity, componentClass:Class<Dynamic>):Void
     {
         if (components.exists(componentClass))
@@ -56,11 +86,19 @@ class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
         }
     }
 
+    /**
+     * Called by the engine when an entity has been rmoved from it. We check if the entity is in
+     * this family's NodeList and remove it if so.
+     */
     public function removeEntity(entity:Entity):Void
     {
         removeIfMatch(entity);
     }
 
+    /**
+     * If the entity is not in this family's NodeList, tests the components of the entity to see
+     * if it should be in this NodeList and adds it if so.
+     */
     private function addIfMatch(entity:Entity):Void
     {
         if (!entities.exists(entity))
@@ -83,6 +121,9 @@ class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
         }
     }
 
+    /**
+     * Removes the entity if it is in this family's NodeList.
+     */
     private function removeIfMatch(entity:Entity):Void
     {
         if (entities.exists(entity))
@@ -102,12 +143,19 @@ class ComponentMatchingFamily<TNode:Node<TNode>> implements IFamily<TNode>
         }
     }
 
+    /**
+     * Releases the nodes that were added to the node pool during this engine update, so they can
+     * be reused.
+     */
     private function releaseNodePoolCache():Void
     {
         engine.updateComplete.remove(releaseNodePoolCache);
         nodePool.releaseCache();
     }
 
+    /**
+     * Removes all nodes from the NodeList.
+     */
     public function cleanUp():Void
     {
         for (node in nodeList)
--- src/ash/core/NodePool.hx
+++ src/ash/core/NodePool.hx
@@ -1,8 +1,12 @@
 package ash.core;
 
 /**
- * This internal class maintains a pool of deleted nodes for reuse by framework. This reduces the overhead
+ * This internal class maintains a pool of deleted nodes for reuse by the framework. This reduces the overhead
  * from object creation and garbage collection.
+ *
+ * Because nodes may be deleted from a NodeList while in use, by deleting Nodes from a NodeList
+ * while iterating through the NodeList, the pool also maintains a cache of nodes that are added to the pool
+ * but should not be reused yet. They are then released into the pool by calling the releaseCache method.
  */
 class NodePool<TNode:Node<TNode>>
 {
@@ -10,11 +14,17 @@ class NodePool<TNode:Node<TNode>>
     private var nodeClass:Class<TNode>;
     private var cacheTail:TNode;
 
+    /**
+     * Creates a pool for the given node class.
+     */
     public function new(nodeClass:Class<TNode>)
     {
         this.nodeClass = nodeClass;
     }
 
+    /**
+     * Fetches a node from the pool.
+     */
     public function get():TNode
     {
         if (tail != null)
@@ -30,6 +40,9 @@ class NodePool<TNode:Node<TNode>>
         }
     }
 
+    /**
+     * Adds a node to the pool.
+     */
     public function dispose(node:TNode):Void
     {
         node.next = null;
@@ -37,12 +50,18 @@ class NodePool<TNode:Node<TNode>>
         tail = node;
     }
 
+    /**
+     * Adds a node to the cache
+     */
     public function cache(node:TNode):Void
     {
         node.previous = cacheTail;
         cacheTail = node;
     }
 
+    /**
+     * Releases all nodes from the cache into the pool
+     */
     public function releaseCache():Void
     {
         while (cacheTail != null)
